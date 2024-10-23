cdf3d2e016e6933086967d74325827a84f6a7b40
shawn-azimov <114064942+shawn-azimov@users.noreply.github.com>
2024-10-23 17:17:51 +0300

    switch to llama 3b model

--- app.py
+++ app.py
@@ -1,64 +1,77 @@
 import gradio as gr
-from huggingface_hub import InferenceClient
+from threading import Thread
+from typing import Iterator
 
-"""
-For more information on `huggingface_hub` Inference API support, please check the docs: https://huggingface.co/docs/huggingface_hub/v0.22.2/en/guides/inference
-"""
-client = InferenceClient("HuggingFaceH4/zephyr-7b-beta")
+from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
+import torch
 
+###
+import os
+from dotenv import load_dotenv
+load_dotenv()
+huggingface_token = os.getenv("HUGGINGFACE_TOKEN")
+###
+
+model_id = "meta-llama/Llama-3.2-3B-Instruct"
+tokenizer = AutoTokenizer.from_pretrained(model_id, token=huggingface_token)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    torch_dtype=torch.bfloat16,
+    token=huggingface_token
+).to(device)
 
 def respond(
-    message,
+    message: str,
     history: list[tuple[str, str]],
-    system_message,
-    max_tokens,
-    temperature,
-    top_p,
-):
-    messages = [{"role": "system", "content": system_message}]
+    system_message: str,
+    max_tokens: int,
+    temperature: float,
+    top_p: float
+) -> Iterator[str]:
 
-    for val in history:
-        if val[0]:
-            messages.append({"role": "user", "content": val[0]})
-        if val[1]:
-            messages.append({"role": "assistant", "content": val[1]})
+    conversation = [{"role": "system", "content": system_message}]
+    
+    for user_msg, assistant_msg in history:
+        if user_msg:
+            conversation.append({"role": "user", "content": user_msg})
+        if assistant_msg:
+            conversation.append({"role": "assistant", "content": assistant_msg})
+    
+    conversation.append({"role": "user", "content": message})
+    
+    input_ids = tokenizer.apply_chat_template(conversation, add_generation_prompt=True, return_tensors="pt")
+    input_ids = input_ids.to(model.device)
 
-    messages.append({"role": "user", "content": message})
+    streamer = TextIteratorStreamer(tokenizer, timeout=20.0, skip_prompt=True, skip_special_tokens=True)
+    
+    generate_kwargs = dict(
+        input_ids=input_ids,
+        streamer=streamer,
+        max_new_tokens=max_tokens,
+        early_stopping=True,
+        do_sample=True,
+        top_p=top_p,
+        temperature=temperature,
+    )
+    
+    t = Thread(target=model.generate, kwargs=generate_kwargs)
+    t.start()
 
     response = ""
-
-    for message in client.chat_completion(
-        messages,
-        max_tokens=max_tokens,
-        stream=True,
-        temperature=temperature,
-        top_p=top_p,
-    ):
-        token = message.choices[0].delta.content
-
+    for token in streamer:
         response += token
         yield response
 
-
-"""
-For information on how to customize the ChatInterface, peruse the gradio docs: https://www.gradio.app/docs/chatinterface
-"""
 demo = gr.ChatInterface(
-    respond,
+    fn=respond,
     additional_inputs=[
         gr.Textbox(value="You are a friendly Chatbot.", label="System message"),
         gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
         gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
-        gr.Slider(
-            minimum=0.1,
-            maximum=1.0,
-            value=0.95,
-            step=0.05,
-            label="Top-p (nucleus sampling)",
-        ),
+        gr.Slider(minimum=0.1, maximum=1.0, value=0.95, step=0.05, label="Top-p (nucleus sampling)"),
     ],
 )
-
 
 if __name__ == "__main__":
     demo.launch()
